9896e4c8db9b9fc5807e3f1290a18a1b0a071658
Danilo Castro <danilogcastro@hotmail.com>
2021-07-12 18:29:08 -0300

    created a service to parse tab separated txt files

---
+++ app/services/file_parser.rb
@@ -0,0 +1,31 @@
+class FileParser
+  def initialize(file)
+    @file = file
+  end
+
+  def parse
+    @headers = []
+    @records = []
+    @file.each_with_index do |line, index|
+      if index.zero?
+        line.split("\t").each { |item| @headers << item }
+      else
+        @records << line.split("\t")
+      end
+    end
+    @structured_records = build_hash(@records)
+  end
+
+  def build_hash(array)
+    array_of_hashes = []
+    array.each do |item|
+      built_hash = {
+        buyer: { name: item[0], address: item[4] },
+        sale: { description: item[1], unit_price: item[2], quantity: item[3] },
+        supplier: item[5].chomp!
+      }
+      array_of_hashes << built_hash
+    end
+    array_of_hashes
+  end
+end
