c1c9738eebda7999d7cd35d0eafb38de10eaabf9
Danilo Castro <danilogcastro@hotmail.com>
2021-07-12 22:04:17 -0300

    installed shoulda match gem

---
+++ spec/controllers/sales_controller.spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper' 
+
+describe SalesController do
+  describe '#create' do
+    it 'deve criar novos registros a partir de uma hash'
+  end
+end
--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper' 
 
 describe Sale do
-  it 'precisa ter uma descrição'
-  it 'precisa contar preço unitário'
-  it 'precisa ter quantidade de itens'
+  subject {
+    described_class.new(description: "Some description",
+                        unit_price: 10.5,
+                        quantity: 3,
+                        buyer: Buyer.new(name: "John Doe", address: "Fake Address"),
+                        supplier: Supplier.new(name: "Fake Supplier"))
+  }
+  it 'precisa ter uma descrição' do
+    expect(subject).to be_valid
+  end
+  it 'precisa contar preço unitário' do
+    expect(subject).to be_valid
+  end
+  it 'precisa ter quantidade de itens' do
+    expect(subject).to be_valid
+  end
+  it 'precisa ter um comprador' do
+    expect(subject).to be_valid
+  end
+  it 'precisa ter um fornecedor' do
+    expect(subject).to be_valid
+  end
 end
