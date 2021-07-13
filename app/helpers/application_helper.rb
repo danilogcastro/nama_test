20490a435d9f6830201eb974a35b419a213c7012
Danilo Castro <danilogcastro@hotmail.com>
2021-07-13 14:05:39 -0300

    improved index view with cards partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sale_total(sale)
+    total = sale.quantity * sale.unit_price
+    number_to_currency(total, unit: "R$", separator: ",", delimiter: ".")
+  end
+
+  def total_gross(sales)
+    sales_array = sales.map do |sale|
+      total = sale.quantity * sale.unit_price
+    end
+    number_to_currency(sales_array.sum, unit: "R$", separator: ",", delimiter: ".")
+  end
 end
